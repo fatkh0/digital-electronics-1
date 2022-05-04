d2343387fb46d8c2963e444b54601a502f500cb8
student <student@PC-635.urel.feec.vutbr.cz>
2022-05-04 16:29:01 +0200

    poject final

---
+++ labs/07-display_driver/project_2.srcs/sources_1/new/top.vhd
@@ -0,0 +1,91 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/29/2022 02:14:42 PM
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           SW : in std_logic_vector(15 downto 0);
+           CA : out STD_LOGIC;
+           CB : out STD_LOGIC;
+           CC : out STD_LOGIC;
+           CD : out STD_LOGIC;
+           CE : out STD_LOGIC;
+           CF : out STD_LOGIC;
+           CG : out STD_LOGIC;
+           DP : out STD_LOGIC;
+           AN : out std_logic_vector(7 downto 0);
+           BTNC : in STD_LOGIC);
+end top;
+
+------------------------------------------------------------
+-- Architecture body for top level
+------------------------------------------------------------
+architecture Behavioral of top is
+  -- No internal signals are needed today:)
+begin
+
+  --------------------------------------------------------
+  -- Instance (copy) of driver_7seg_4digits entity
+  driver_seg_4 : entity work.driver_7seg_4digits
+       port map(
+            clk        => CLK100MHZ,
+            reset      => BTNC,
+            
+            data0_i(3) => SW(3),
+            data0_i(2) => SW(2),
+            data0_i(1) => SW(1),
+            data0_i(0) => SW(0),
+            
+            data1_i(3) => SW(7),
+            data1_i(2) => SW(6),
+            data1_i(1) => SW(5),
+            data1_i(0) => SW(4),
+            
+            
+            dp_i => "0111",
+            dp_o => DP,
+            
+            seg_o(6) => CA,
+            seg_o(5) => CB,
+            seg_o(4) => CC,
+            seg_o(3) => CD,
+            seg_o(2) => CE,
+            seg_o(1) => CF,
+            seg_o(0) => CG,
+            
+            dig_o => AN(4 - 1 downto 0)
+        );
+
+     -- Disconnect the top four digits of the 7-segment display
+    AN(7 downto 4) <= b"1111";
+
+end architecture Behavioral;
--- labs/08-traffic_lights/project_2.srcs/sources_1/new/tlc.vhd
+++ labs/08-traffic_lights/project_2.srcs/sources_1/new/tlc.vhd
@@ -31,7 +31,7 @@ entity tlc is
         clk     : in  std_logic;
         reset   : in  std_logic;
         -- Traffic lights (RGB LEDs) for two directions
-        go_o : out std_logic
+        south_o : out std_logic
     );
 end entity tlc;
 
@@ -79,7 +79,7 @@ architecture Behavioral of tlc is
     -- Local delay counter
     signal s_cnt : unsigned(4 downto 0);
     
-    signal counter: integer := 0;
+    signal counter: natural := 0;
 
     -- Specific values for local counter
     constant c_DELAY_7SEC : unsigned(4 downto 0) := b"1_1100";
@@ -91,6 +91,7 @@ architecture Behavioral of tlc is
 
     -- Output values
     constant c_RED        : std_logic_vector(2 downto 0) := b"100";
+    constant c_NULL        : std_logic_vector(2 downto 0) := b"000";
     
 
 begin
@@ -122,8 +123,9 @@ begin
     begin
         if rising_edge(clk) then
             if (reset = '1') then   -- Synchronous reset
-                s_state <= STOP1;   -- Set initial state
+                s_state <= A;   -- Set initial state
                 s_cnt   <= c_ZERO;  -- Clear delay counter
+                counter <= 0;
 
             elsif (s_en = '1') then
                 -- Every 250 ms, CASE checks the value of the s_state 
@@ -138,6 +140,8 @@ begin
                         -- Count up to c_DELAY_1SEC
                         if (s_cnt < DOT and counter = 0) then
                             s_cnt <= s_cnt + 1;
+                            s_state <= A;
+                            counter <= counter;
                         else
                             -- Move to the next state
                             s_state <= A;
@@ -148,6 +152,8 @@ begin
                         
                         if (s_cnt < c_DELAY_1SEC and counter = 1) then
                             s_cnt <= s_cnt + 1;
+                            s_state <= A;
+                            counter <= counter;
                         else
                             -- Move to the next state
                             s_state <= A;
@@ -158,6 +164,8 @@ begin
                         
                         if (s_cnt < DASH and counter = 2) then
                             s_cnt <= s_cnt + 1;
+                            s_state <= A;
+                            counter <= counter;
                         else
                             -- Move to the next state
                             s_state <= A;
@@ -168,6 +176,8 @@ begin
                         
                         if (s_cnt < c_DELAY_3SEC and counter = 3) then
                             s_cnt <= s_cnt + 1;
+                            s_state <= A;
+                            counter <= counter;
                         else
                             -- Move to the next state
                             s_state <= STOP1;
@@ -258,12 +268,50 @@ begin
                         end if;
                       -------------------------------------------------------------------------------------------      
                       
-                      when C =>
-                          
+                     
 
                     when others =>
-                        s_state <= STOP1;
-                        s_cnt   <= c_ZERO;
+                    if (s_cnt < DOT and counter = 0) then
+                            s_cnt <= s_cnt + 1;
+                        else
+                            -- Move to the next state
+                            s_state <= A;
+                            counter <= counter + 1;
+                            -- Reset local counter value
+                            s_cnt <= c_ZERO;
+                        end if;
+                        
+                        if (s_cnt < c_DELAY_1SEC and counter = 1) then
+                            s_cnt <= s_cnt + 1;
+                        else
+                            -- Move to the next state
+                            s_state <= A;
+                            counter <= counter + 1;
+                            -- Reset local counter value
+                            s_cnt <= c_ZERO;
+                        end if;
+                        
+                        if (s_cnt < DASH and counter = 2) then
+                            s_cnt <= s_cnt + 1;
+                        else
+                            -- Move to the next state
+                            s_state <= A;
+                            counter <= counter + 1;
+                            -- Reset local counter value
+                            s_cnt <= c_ZERO;
+                        end if;
+                        
+                        if (s_cnt < c_DELAY_3SEC and counter = 3) then
+                            s_cnt <= s_cnt + 1;
+                        else
+                            -- Move to the next state
+                            s_state <= STOP1;
+                            counter <= 0;
+                            -- Reset local counter value
+                            s_cnt <= c_ZERO;
+                        end if;
+                        --s_state <= STOP1;
+                        --s_cnt   <= c_ZERO;
                 end case;
             end if; -- Synchronous reset
         end if; -- Rising edge
@@ -280,14 +328,17 @@ begin
     begin
         case s_state is
             when STOP1 =>
-                go_o <= c_GREEN;
+                south_o <= c_NULL;
                 
             when A =>
-                go_o <= c_RED;
+                south_o <= c_RED;
+                
+            when B =>
+                south_o <= c_RED;
                 
             
             when others =>
-                go_o <= c_RED;
+                south_o <= c_NULL;
         end case;
     end process p_output_fsm;
 
